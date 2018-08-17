3b07a400a54c0c12676209f0ccf6b53e48fbc863
Osama Hassanein <player1os@gmail.com>
2018-08-17 12:48:02 +0200

    A a basis for testing

---
+++ test/index.test.js
@@ -0,0 +1,21 @@
+test('Placeholder', () => {
+	/*
+	let x = 42
+	if (false) {
+		x = -1
+	}
+
+	interface IQ {
+		z?: number
+	}
+
+	const z: IQ = {}
+
+	console.log(z, Promise.promisify)
+
+	import BaseError from '.../../Personal/node-js-base-error'
+
+	const b = new BaseError('alas' + x)
+	throw b
+	*/
+})
